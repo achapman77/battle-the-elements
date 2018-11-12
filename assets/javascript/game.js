aa629049b0001569468fc411542e803c498f9e7b
AChapman <achapman77@gmail.com>
2018-11-12 13:42:49 -0700

    Added more responsive Bootstrap classes and minor CSS changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,47 +6,49 @@ $(function() {
     var losses = 0;
     var counter = 0;
     var targetNumber = randomValue(120, 19);
-    var winMessage = "You Win!"
-    var loseMessage = "You Lose!"
+    var winMessage = "YOU WIN!"
+    var loseMessage = "YOU LOSE!"
 
-    $("#target-number").text(targetNumber);
-    $("#wins").text(wins);
-    $("#losses").text(losses);
-    $("#counter").text(counter);
-
-
+    
     var imageArray = ["assets/images/fire.png",
     "assets/images/water.png",
     "assets/images/air.png",
     "assets/images/earth.png"];
 
-    //function that could take in two ints and pass these to our math.random
 
+    //Displays score board vaules
+    function displayScoreBoard() {
+    $("#target-number").text(targetNumber);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    // $("#counter").text(counter);
+    }
 
-
-    function reset() {
-        counter = 0;
-        $("#counter").text(counter);
-        targetNumber = randomValue(120,19);
-        $("#target-number").text(targetNumber);
-        // for (var i = 0; i < imageArray.length; i++) {
-        // imageElement.removeAttr("data-elementvalue");
-        // };
-        deleteElements();
-        // displayElements();
-
-    };
-
+    //Generates random numbers for both target number and values assiged to Elementals.
     function randomValue(max, min) {
         return Math.floor((Math.random() * (max - min)+1));
     };
 
-    function deleteElements () {
+    //Deletes images & attr to clear way for new images & attr.
+    // Called in reset function below
+    function deleteElementals () {
         for (var i = 0; i < imageArray.length; i++) {
             $("#elements img").remove();
         };
     }
 
+    function resetGame() {
+        var counter = 0;
+        $("#counter").text(counter);
+
+        targetNumber = randomValue(120,19);
+        $("#target-number").text(targetNumber);
+
+        $("#elements img").remove();
+        displayElements();
+    };
+
+    //Adds images and assigns randome values to images
     function displayElements () {
         for (var i = 0; i < imageArray.length; i++) {
         
@@ -60,9 +62,10 @@ $(function() {
             
         };
     }
-
-    displayElements();
-    // reset();
+    
+    displayScoreBoard();
+    resetGame()
+    
 
     $(".element-image").on("click", function() {
         var elementValue = ($(this).attr("data-elementvalue"));
@@ -76,15 +79,15 @@ $(function() {
             wins++;
             $("#wins").text(wins);
             $("#game-message").text(winMessage);
-            reset();
-            displayElements();
+            resetGame();
+            // displayElements();
             // alert("you win");
         } else if (counter >= targetNumber) {
             losses++;
             $("#losses").text(losses);
             $("#game-message").text(loseMessage);
-            reset();
-            displayElements();
+            resetGame();
+            // displayElements();
             // alert("you lose");
         }
     });
